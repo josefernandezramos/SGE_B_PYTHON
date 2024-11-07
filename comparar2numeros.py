bc011f55e7fe47e8b9c9f90c3626f5f759ff985e
josefernandezramos <josegfernandezramos@gmail.com>
2024-11-07 12:01:42 +0100

    exercici comparar2numeros.py finalitzat

---
+++ comparar2numeros.py
@@ -0,0 +1,9 @@
+num1 = 5
+num2 = 3
+
+if num1 == num2:
+    print("Els 2 números son iguals")
+elif num1 > num2:
+    print(f"El num1 {num1} és més gran que el num2 {num2}")
+else:
+    print(f"El num2 {num2} és més gran que el num1 {num1}")
