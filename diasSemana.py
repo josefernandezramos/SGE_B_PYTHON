a3fbb6589c1f0e4651f5976b0c7938d5eef57c0a
josefernandezramos <josegfernandezramos@gmail.com>
2024-11-07 12:19:27 +0100

    exercici dies setmana finalitzat

---
+++ diasSemana.py
@@ -0,0 +1,19 @@
+dia_setmana = ""
+
+dia_setmana = input("Ingresa un día: ")
+
+
+if dia_setmana == "dilluns":
+    print("Avui és dilluns")
+elif dia_setmana == "dimarts":
+    print("Avui és dimarts")
+elif dia_setmana == "dimecres":
+    print("Avui és dimecres")
+elif dia_setmana == "dijous":
+    print("Avui és dijous")
+elif dia_setmana == "divendres":
+    print("Avui és divendres")
+elif dia_setmana == "disabte":
+    print("Avui és disabte")
+elif dia_setmana == "diumenge":
+    print("Avui és diumenge")
