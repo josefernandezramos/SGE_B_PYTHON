81d05344722e8b12984454ed0346a7ab1d3084ed
josefernandezramos <josegfernandezramos@gmail.com>
2024-11-19 15:23:34 +0100

    imprimir elements llista amb for

---
+++ Activitat3/imprimElementLista_for.py
@@ -0,0 +1,8 @@
+'''
+Imprimir els elements d'una llista amb for. fruits = [“poma”,”pera”,”raïm”,”plàtan”]
+'''
+
+fruits = ["poma","pera","raïm","plàtan"]
+
+for fruit in fruits:
+    print(f'{fruit}')
